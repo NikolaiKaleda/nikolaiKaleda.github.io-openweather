462a44f884cab10c2f1e9098fd12fdd10d18ce56
home\Nikolai <nikolai.kaleda@gmail.com>
2018-01-31 14:10:43 +0300

    Add set interval and stop interval.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,21 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-
-
-
     var input = document.querySelector('.formSubmit');
+    var stop = document.querySelector('.stopInterval');
+    var interval = null;    
 
-    input.addEventListener('click', function (ev) {
+    //var test = setInterval(clickListener, 1000);
+
+    input.addEventListener('click', function () {
+        interval = setInterval(clickListener, 2000);
+    });
+
+    function clickListener() {
         var latitude = document.querySelector('#latitude').value;
         var longitude = document.querySelector('#longitude').value;
-
         if (latitude == "" || longitude == "") {
             getCurrentLocation();
         } else {
             checkValid(latitude, longitude);
         }
-    });
-
+    }
 
     function getCurrentLocation() {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -25,8 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     };
 
-
-    function checkValid (latitude, longitude) {
+    function checkValid(latitude, longitude) {
         var regCoord = /-?\d{1,3}\.\d+/;
         if (regCoord.test(latitude) && regCoord.test(longitude)) {
             submitForm(latitude, longitude);
@@ -35,28 +37,32 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-
     function submitForm(latitude, longitude) {
         getData(latitude, longitude);
     }
-
-
-
-
-
-
-
-
 
     function getData(latitude, longitude) {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '139&APPID=79db73bf448b423ff3b6dd802029981d', false);
         xhr.send();
         if (xhr.status != 200) {
-            alert(xhr.status + ', ' + xhr.statusText); // пример вывода: 404: Not Found
+            alert(xhr.status + ', ' + xhr.statusText);
         } else {
-            //                    alert(JSON.parse(xhr.responseText)); // responseText -- текст ответа.
             console.log(JSON.parse(xhr.responseText));
         }
     };
+
+
+
+    
+    
+    
+    stop.addEventListener('click', function () {
+        clearInterval(interval );
+//        console.log(stopInterval);
+    });
+
+
+
+
 });
